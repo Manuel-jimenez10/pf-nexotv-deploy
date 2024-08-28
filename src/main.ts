59bd57d44227ee1ca4be4e9c17882c28b5a89017
Manuel-jimenez10 <136191459+Manuel-jimenez10@users.noreply.github.com>
2024-08-28 13:07:18 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule,  {
+  cors: {
+    origin: ['https://nexo-tv.vercel.app', 'http://localhost:3000', 'http://localhost:3001'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  },
+});
 
   app.useGlobalPipes(
     new ValidationPipe({
