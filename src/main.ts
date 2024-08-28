e4dffcfd76372e6a65df10b94146987320f7e630
Manuel-jimenez10 <136191459+Manuel-jimenez10@users.noreply.github.com>
2024-08-28 17:40:08 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,28 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'http://localhost:3000',
+        'https://nexo-tv.vercel.app',
+        'http://localhost:3001',
+        // Añade aquí cualquier otro dominio desde el que necesites hacer solicitudes
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders: 'Content-Type,Accept,Authorization',
+    },
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-app.enableCors({
-    origin: ['https://nexo-tv.vercel.app','http://localhost:3000'],
-    allowedHeaders: 'Content-Type, Accept',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-  await app.listen(3000);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
